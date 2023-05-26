d0a64067cf47f6aefe4b986e907d8a2c736c581d
AlexCaprian <100376334+AlexCaprian@users.noreply.github.com>
2023-05-26 19:31:06 -0300

    Novo

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.getElementById("radio1").checked = true;
 
 setInterval(  function(){
     nextImage();
-}, 2000)
+}, 3000)
 
 function nextImage(){
     count++;
-    if(count>5){
+    if(count>4){
         count = 1;
     }
     document.getElementById("radio"+count).checked = true;
